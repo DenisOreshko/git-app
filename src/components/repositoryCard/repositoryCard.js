4abbd35e666f743a3d016d89ce66ae5dc33ae9a2
Denis <denis7371@gmail.com>
2022-05-17 11:37:37 +0200

    optimise transformDescription()

--- src/components/repositoryCard/repositoryCard.js
+++ src/components/repositoryCard/repositoryCard.js
@@ -12,41 +12,43 @@ class RepositoryCard extends Component {
         }))      
     }
     
+    hidePartDescription = (describe, limit , atext) => {
+        if(describe.length > limit){
+            describe = "" + describe.slice(0, limit);
+            atext = ' ...more';
+        }
+        return {describe, atext};
+    }
+
+    showAllDescription = (describe, limit, atext) => {
+        if(describe.length > limit){
+            atext = '  hide';
+        }        
+        return {describe, atext}
+    }
+
     //hide or show more Description character limit
     transformDescription = (description, showAllDescription, limit) => {
-        let descrShow = null;
-        let atext = null;
-
-        if(description !== null){
+        if(description !== null){           
             if(showAllDescription){
-
-                descrShow = description;
-                if(description.length > limit){atext = '  hide';} 
-
+                return this.showAllDescription(description, limit, null);
             }else{
-
-                if(description.length > limit){
-                    descrShow = "" + description.slice(0, limit);
-                    atext = ' ...more';
-                }else{
-                    descrShow = description;
-                } 
-                
+                return this.hidePartDescription(description, limit, null);             
             }
-        }
-        return {descrShow, atext} 
+        }        
+        return {describ:null, atext:null};      
     }
 
     render(){
         const {name, html_url,description} = this.props;
         const {showAllDescription} = this.state;       
-        const {descrShow, atext} = this.transformDescription(description, showAllDescription, 180);        
+        const {describe, atext} = this.transformDescription(description, showAllDescription, 180);        
 
         return (
             <div className="repositories-card">
                     <a href={html_url} target="_blank" rel="noreferrer">{name}</a>
                     <span className="repository-info">
-                        {descrShow} <a href="#0" className='showMore' onClick={this.showAllDescriptionToggle}>{atext}</a>
+                        {describe} <a href="#0" className='showMore' onClick={this.showAllDescriptionToggle}>{atext}</a>
                     </span>                    
             </div>
         )
